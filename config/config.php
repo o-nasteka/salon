939aadf129d1e452ded54bd82f333b97f25623fe
Oleg Nasteka <oleg.nasteka@gmail.com>
2015-09-09 20:56:10 +0300

    Admin edit

--- config/config.php
+++ config/config.php
@@ -9,18 +9,3 @@ Config::set('routes', array(
     'default' => '',
     'admin'   => 'admin_',
 ));
-
-
-Config::set('default_route', 'default');
-Config::set('default_language', 'en');
-Config::set('default_controller', 'pages');
-Config::set('default_action', 'index');
-
-Config::set('db.host', 'localhost');
-Config::set('db.user', 'salon');
-Config::set('db.password', 'Z4x5VaJ6GQcT2JVB');
-Config::set('db.db_name', 'salon');
-
-Config::set('salt', 'jd7sj3sdkd964he7e');
-
-// comments
